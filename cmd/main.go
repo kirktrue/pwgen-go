8cca68a55d9eefe71cdcdac17ef017a3f3133577
Kirk True <kirk@mustardgrain.com>
2018-03-02 13:17:51 -0800

    updates

--- cmd/main.go
+++ cmd/main.go
@@ -8,37 +8,101 @@ import (
 	"os"
 	"time"
 
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
-const FILE_NAME = "/usr/share/dict/words"
+const DEFAULT_WORDS_FILE_NAME = "/usr/share/dict/words"
+const DEFAULT_MAX_LENGTH = 64
 
 var rootCmd = &cobra.Command{
 	Use:   "pwgen",
-	Short: "pwgen generates passwords from " + FILE_NAME,
-	Run:   ff,
+	Short: "pwgen generates passwords from the specified (or default) file",
+	Run:   run,
 }
+var wordsFileName string
+var maxLength int
+var verbose bool
 
-func ff(cmd *cobra.Command, args []string) {
-	f, err := os.Open(FILE_NAME)
+func run(cmd *cobra.Command, args []string) {
+	f, err := os.Open(wordsFileName)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
 	lines, err := getLines(f)
+
 	if err != nil {
 		panic(err)
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	index := rand.Intn(len(lines))
-	fmt.Printf("Count: %d\n", len(lines))
-	fmt.Printf("Line at index %d: %s\n", index, lines[index])
+	maxWords := 4
+	maxTries := 20
+	availableLength := maxLength - 2 // maxWords is also number of trailing dashes, 2 is the digit suffix
+
+	var words = make([]string, 0)
+
+	for i := 0; i < maxTries; i++ {
+		index := rand.Intn(len(lines))
+		word := lines[index]
+
+		if verbose {
+			fmt.Printf("availableLength: %d, candidate word: %s\n", availableLength, word)
+		}
+
+		wordLength := len(word) + 1
+
+		if wordLength > availableLength {
+			// Candidate word rejected for being too long
+			continue
+		}
+
+		words = append(words, word)
+		availableLength -= wordLength
+
+		if verbose {
+			fmt.Printf("Remaining availableLength: %d\n", availableLength)
+		}
+
+		if len(words) >= maxWords || availableLength <= 4 {
+			break
+		}
+	}
+
+	var pwd = ""
+
+	for i, word := range words {
+		if i == 0 {
+			word = strings.Title(word)
+		} else {
+			pwd += "-"
+		}
+
+		pwd += word
+	}
+
+	var suffix = 0
+
+	for {
+		suffix = rand.Intn(100)
+
+		// We don't want a suffix of "00", so let's skip 0 if we get it from the
+		// random number generator.
+		if suffix > 0 {
+			break
+		}
+	}
+
+	pwd = fmt.Sprintf("%s-%02d", pwd, suffix)
+
+	fmt.Println(pwd)
 }
 
 func getLines(r io.Reader) ([]string, error) {
-	var a = make([]string, 0)
+	var lines = make([]string, 0)
 	reader := bufio.NewReader(r)
 
 	for {
@@ -46,13 +110,13 @@ func getLines(r io.Reader) ([]string, error) {
 
 		switch {
 		case err == io.EOF:
-			return a, nil
+			return lines, nil
 
 		case err != nil:
 			return nil, err
 		}
 
-		a = append(a, line)
+		lines = append(lines, strings.TrimSpace(line))
 	}
 }
 
@@ -64,5 +128,7 @@ func Execute() {
 }
 
 func init() {
-	//rootCmd.AddCommand(rootCmd)
+	rootCmd.Flags().StringVarP(&wordsFileName, "file-name", "f", DEFAULT_WORDS_FILE_NAME, "File containing words from which to choose")
+	rootCmd.Flags().IntVarP(&maxLength, "max-length", "m", DEFAULT_MAX_LENGTH, "Maximum length of the password")
+	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Verbose output (useful for debugging, mostly)")
 }
